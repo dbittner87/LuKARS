c4876031c81503c281a8c63c5ade90da93d4380a
dbittner87 <daniel.bittner@tum.de>
2018-09-04 14:15:41 +0200

    Update interception_losses.py

--- interception_losses.py
+++ interception_losses.py
@@ -1,6 +1,7 @@
 def interception_losses(model_period):
     
     import pandas as pd
+    
     I_loss = pd.Series([0.11, 0.12, 0.13, 0.14, 0.15, 0.16, 0.17, 0.16, 0.15, 0.14, 0.13, 0.12])
     daily_losses = pd.Series(index = model_period)
     monthly_losses = daily_losses.groupby(pd.Grouper(freq = 'M')).mean()
@@ -15,6 +16,5 @@ def interception_losses(model_period):
     monthly_losses.index = days
     x = pd.Series(monthly_losses, index = day_seq)
     x = x.interpolate()
-    daily_losses = x[0:len(model_period)]
-    daily_losses.index = range(len(model_period))
+    daily_losses = list(x[0:len(model_period)])
     return(daily_losses);
